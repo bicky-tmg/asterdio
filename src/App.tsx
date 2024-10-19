fb9c428f623a2a7e1272121161d337a80c45d017
Bicky Tamang <bickytamang101@gmail.com>
2024-10-20 00:21:25 +0545

    feat: section title and product grid responsive

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,31 @@ import styled from "styled-components";
 import RootLayout from "./components/layout/RootLayout";
 import { GlobalStyle } from "./components/styles/globalStyle";
 import Card from "./components/ui/card";
+import { SectionTitle } from "./components/styles/sectionTitle";
 
 const GridContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   gap: 2rem;
+
+  @media (max-width: 1024px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (max-width: 640px) {
+    grid-template-columns: 1fr;
+  }
 `;
 
 function App() {
   return (
     <RootLayout>
       <GlobalStyle />
-      <h1>Root page</h1>
+      <SectionTitle>All Products</SectionTitle>
       <GridContainer>
         <Card
           imgSrc="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
---
+++ src/components/styles/sectionTitle.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const SectionTitle = styled.h2`
+  font-size: 1.875rem;
+  line-height: 2.25rem;
+  font-weight: 700;
+  margin-top: 3.5rem;
+  margin-bottom: 2.5rem;
+
+  @media (min-width: 1024px) {
+    font-size: 2.25rem;
+    line-height: 2.5rem;
+  }
+`;
